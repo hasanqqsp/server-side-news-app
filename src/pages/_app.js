f335c265b364861c1c1845f7b11f7ed9a39fec6e
Hasan Ismail <hasanqqsp@gmail.com>
2023-11-18 04:47:17 +0700

    deploy

---
+++ src/pages/_app.js
@@ -0,0 +1,7 @@
+// import '@/styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.css'
+// import * as bootstrap from 'bootstrap'
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
